0498a69816edb2d29c5731bc718792d356ba160a
RohanshresthaGit <150516010+RohanshresthaGit@users.noreply.github.com>
2026-05-04 22:41:48 +0545

    add: either utils.

---
+++ lib/core/utils/either.dart
@@ -0,0 +1,48 @@
+/// A simple FP-style Either implementation (fpdart-like)
+/// Left = Failure, Right = Success
+library;
+
+sealed class Either<L, R> {
+  const Either();
+
+  T fold<T>(
+    T Function(L l) onLeft,
+    T Function(R r) onRight,
+  );
+}
+
+/// -------------------- LEFT (Failure) --------------------
+class Left<L, R> extends Either<L, R> {
+  final L value;
+
+  const Left(this.value);
+
+  @override
+  T fold<T>(
+    T Function(L l) onLeft,
+    T Function(R r) onRight,
+  ) {
+    return onLeft(value);
+  }
+
+  @override
+  String toString() => 'Left($value)';
+}
+
+/// -------------------- RIGHT (Success) --------------------
+class Right<L, R> extends Either<L, R> {
+  final R value;
+
+  const Right(this.value);
+
+  @override
+  T fold<T>(
+    T Function(L l) onLeft,
+    T Function(R r) onRight,
+  ) {
+    return onRight(value);
+  }
+
+  @override
+  String toString() => 'Right($value)';
+}
