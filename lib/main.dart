310ac0597ababd3e29d81f683c8e691fe8a29205
David Kamau <119078443+Pak-Man926@users.noreply.github.com>
2026-05-15 13:33:02 +0300

    Splitting of the app entry file from the main file to ensure separation of concerns

--- lib/main.dart
+++ lib/main.dart
@@ -1,50 +1,11 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:vanashree_ngo_application/app.dart';
 
-import 'config/localization/l10n/app_localizations.dart';
-import 'config/router/app_router.dart';
-import 'config/theme/app_theme.dart';
-import 'config/theme/providers/theme_provider.dart';
-import 'features/localization/presentation/providers/language_view_model.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(const ProviderScope(child: App()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return Consumer(
-      builder: (context, ref, child) {
-        final localeState = ref.watch(languageViewModelProvider);
-        final locale = localeState.value != null
-            ? Locale(localeState.value!.code)
-            : const Locale('en');
-        return MaterialApp.router(
-          debugShowCheckedModeBanner: false,
-          title: 'Vanashree',
-          localizationsDelegates: const [
-            AppLocalizations.delegate,
-            GlobalMaterialLocalizations.delegate,
-            GlobalWidgetsLocalizations.delegate,
-            GlobalCupertinoLocalizations.delegate,
-          ],
-          supportedLocales: const [
-            Locale('en'), // English
-            Locale('hi'), // Hindi
-            Locale('mr'), // Marathi
-          ],
-          locale: locale,
-          theme: AppTheme.lightTheme(),
-          darkTheme: AppTheme.darkTheme(),
-          themeMode: ref.watch(themeModeProvider),
-          routerConfig: router,
-        );
-      },
-    );
-  }
-}
